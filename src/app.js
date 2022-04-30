b30da754188273e7d9c4473301935e4a778455ac
umangkumarr <duhoon44@gmail.com>
2022-04-30 13:22:13 +0530

    intial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express();
+const ejs = require("ejs");
+const path = require("path")
+
+app.use(express.static(path.join(__dirname, "../public")));
+app.set('view engine', "ejs");
+
+
+app.get("/home", (req, res) => {
+    res.render("home");
+})
+
+app.listen(5500);
