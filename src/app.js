db213d2801e136e2ffb5ae5776961165d770ec21
umangkumarr <duhoon44@gmail.com>
2022-05-03 17:53:42 +0530

    Added Procfile

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express();
 const ejs = require("ejs");
 const path = require("path")
+const PORT = process.env.PORT || 8000
 
 app.use(express.static(path.join(__dirname, "../public")));
 app.set('view engine', "ejs");
